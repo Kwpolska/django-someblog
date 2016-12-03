48b1e9c77e776558427d408bcb233b5069222770
Chris Warrick <kwpolska@gmail.com>
2016-12-03 14:59:23 +0100

    Fix admin registrations and char/text fields
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- someblog/admin.py
+++ someblog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from someblog.models import Post, Tag, Author
 
-admin.register(Post)
-admin.register(Tag)
-admin.register(Author)
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Author)
--- someblog/models.py
+++ someblog/models.py
@@ -2,20 +2,20 @@ from django.conf import settings
 from django.db import models
 
 class Tag(models.Model):
-    name = models.TextField(max_length=100)
+    name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
 
 
 class Author(models.Model):
-    name = models.TextField(max_length=100)
+    name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
     bio = models.TextField()
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 
 class Post(models.Model):
-    title = models.TextField(max_length=100)
+    title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
-    content = models.TextField()
+    text = models.TextField()
     tags = models.ManyToManyField(Tag)
     author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
